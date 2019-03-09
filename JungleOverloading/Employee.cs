715f22f484d4296b1d5f7cc5ebce85124f6b2e9f
unknown <c.chase.hamby@gmail.com>
2019-03-09 11:00:20 -0600

    it works!

---
+++ JungleOverloading/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleOverloading
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set;  }
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        static List<string> restaurants = new List<string>
+        {
+            "City House",
+            "Peter's Sushi",
+            "Subculture",
+            "Martin's BBQ"
+        };
+
+        public string randomRestaurant()
+        {
+            var Random = new Random();
+            int r = Random.Next(restaurants.Count);
+            return((string)restaurants[r]);
+        }
+
+        public void eat()
+        {
+            Console.WriteLine($"I saw {FirstName} {LastName} eating at {randomRestaurant()} yesterday!");
+        }
+
+        public void eat(string food)
+        {
+            Console.WriteLine($"I saw {FirstName} {LastName} eating {food} at {randomRestaurant()} yesterday!");
+        }
+
+        public void eat(List<Employee> companions)
+        {
+            //eat();
+            foreach (var employee in companions)
+            {
+                Console.WriteLine($"I'm eating with {employee.FirstName} at {randomRestaurant()}!");
+            }
+        }
+
+        public void eat(string food, List<Employee> companions)
+        {
+            Console.WriteLine($"I am eating {food}");
+            eat(companions);
+        }
+    }
+}
---
+++ JungleOverloading/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JungleOverloading
+{
+    class Program
+    {
+        public static List<Employee> Employees { get; set; } = new List<Employee>();
+
+        static void Main(string[] args)
+        {
+            var chase = new Employee("Chase", "Hamby");
+            var wayne = new Employee("Wayne", "Collier");
+            var leo = new Employee("Leo", "Hamby");
+
+            Employees.Add(chase);
+            Employees.Add(wayne);
+            Employees.Add(leo);
+
+            chase.eat();
+            chase.eat("burgers");
+            chase.eat(Employees);
+            chase.eat("burgers", Employees);
+
+
+            Console.ReadKey();
+        }
+    }
+}
